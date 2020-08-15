85ad46400fdb47d438099697e99b4198678a4e1a
Sebastian Berns <s.berns@qmul.ac.uk>
2020-08-15 13:09:25 +0100

    Adapt to mixing from z codes

--- style_mix_two_projections.py
+++ style_mix_two_projections.py
@@ -1,27 +1,29 @@
 import argparse
-import math
-import torch
+from pathlib import Path
 import os
-import random
 
+import torch
 from torchvision import utils
 from model import Generator
 from tqdm import tqdm
 
-def mix_styles(args, generator, l1, l2, n1, n2):
+def mix_styles(args, generator, l1, l2):
     with torch.no_grad():
         generator.eval()
         slice_l1 = l1[0,:].unsqueeze(0)
         slice_l2 = l2[0,:].unsqueeze(0)
-        
-        for i in range(16):
-            image, _ = generator([slice_l1,slice_l2], input_is_latent=True, inject_index=i)
-            if not os.path.exists('sample'):
-                os.makedirs('sample')
+
+        for i in tqdm(range(1, generator.n_latent-1)):
+            image, _ = generator([slice_l1,slice_l2],
+                input_is_latent=False,  # IMPORTANT CHANGE
+                inject_index=i)
+
+            save_dir = Path(args.save_dir)
+            save_dir.mkdir(parents=True, exist_ok=True)
 
             utils.save_image(
                 image,
-                f'sample/style_mix_{str(i).zfill(2)}.png',
+                save_dir/f'style_mix_{str(i).zfill(2)}.png',
                 nrow=1,
                 normalize=True,
                 range=(-1, 1),
@@ -34,8 +36,9 @@ if __name__ == '__main__':
     parser.add_argument('--size', type=int, default=1024)
     parser.add_argument('--channel_multiplier', type=int, default=2)
     parser.add_argument('--ckpt', type=str, default="models/stylegan2-ffhq-config-f.pt")
-    parser.add_argument('--latent1', type=str, default="") 
-    parser.add_argument('--latent2', type=str, default="") 
+    parser.add_argument('--latent1', type=str, default="")
+    parser.add_argument('--latent2', type=str, default="")
+    parser.add_argument('--save_dir', type=str, default="sample/")
 
     args = parser.parse_args()
 
@@ -47,11 +50,8 @@ if __name__ == '__main__':
     ).to(device)
     checkpoint = torch.load(args.ckpt)
     generator.load_state_dict(checkpoint['g_ema'])
- 
+
     l1 = torch.load(args.latent1)['latent']
     l2 = torch.load(args.latent2)['latent']
-    
-    n1 = torch.load(args.latent1)['noises']
-    n2 = torch.load(args.latent2)['noises']
 
-    mix_styles(args, generator, l1, l2, n1, n2)
+    mix_styles(args, generator, l1, l2)
