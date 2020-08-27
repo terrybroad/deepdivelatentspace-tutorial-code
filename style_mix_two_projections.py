4d11fa8125f32e8390a0d5fe231a24897935b828
Terence Broad <terry.m.broad@gmail.com>
2020-08-27 18:17:08 +0100

    Save figure as strip of samples

--- style_mix_two_projections.py
+++ style_mix_two_projections.py
@@ -8,24 +8,28 @@ from torchvision import utils
 from model import Generator
 from tqdm import tqdm
 
+
 def mix_styles(args, generator, l1, l2, n1, n2):
     with torch.no_grad():
         generator.eval()
         slice_l1 = l1[0,:].unsqueeze(0)
         slice_l2 = l2[0,:].unsqueeze(0)
         
-        for i in range(16):
+        images = []
+        for i in range(1,16,2):
             image, _ = generator([slice_l1,slice_l2], input_is_latent=True, inject_index=i)
             if not os.path.exists('sample'):
                 os.makedirs('sample')
+            images.append(image)
 
-            utils.save_image(
-                image,
-                f'sample/style_mix_{str(i).zfill(2)}.png',
-                nrow=1,
-                normalize=True,
-                range=(-1, 1),
-            )
+        image_out = torch.cat(images)
+        utils.save_image(
+            image_out,
+            f'sample/style_mix_'+args.label+'.png',
+            nrow=8,
+            normalize=True,
+            range=(-1, 1)
+        )
 
 if __name__ == '__main__':
     device = 'cuda'
@@ -33,9 +37,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--size', type=int, default=1024)
     parser.add_argument('--channel_multiplier', type=int, default=2)
-    parser.add_argument('--ckpt', type=str, default="models/stylegan2-ffhq-config-f.pt")
+    parser.add_argument('--ckpt', type=str, default="ckpt/stylegan2-ffhq.pt")
     parser.add_argument('--latent1', type=str, default="") 
-    parser.add_argument('--latent2', type=str, default="") 
+    parser.add_argument('--latent2', type=str, default="")
+    parser.add_argument('--label', type=str, default="fig1") 
 
     args = parser.parse_args()
 
